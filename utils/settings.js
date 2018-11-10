d1e5227fa5061347022117620d9dc710989fd21e
Vinay <vramesh@princeton.edu>
2018-11-10 02:55:06 -0500

    Update utils/settings.js

---
+++ utils/settings.js
@@ -0,0 +1,3 @@
+export const API_PORT = 5000
+export const API_SERVER = 'https://campusdining.princeton.edu/dining/_Foodpro/menu2.asp?myaction=read'
+export const API_URL = API_SERVER + ":" + API_PORT
