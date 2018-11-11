94b94606443f4fc7591fd2c1ad563a925544c75c
Vinay <vramesh@princeton.edu>
2018-11-10 21:26:00 -0500

    API fix and News Scrolling through date

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -7,42 +7,114 @@ import {
     FlatList
 } from 'react-native';
 
-import {DATABASE_URL, SCREEN_WIDTH} from '../utils/settings' 
+import {DATABASE_URL, SCREEN_WIDTH, SCREEN_HEIGHT} from '../utils/settings' 
 
 export default class NewsScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            dtdate: new Date().getDay(),
+            dtdate: new Date(),
             news_stories: null
         }
     }
     async componentDidMount() {
-        let day = this.state.dtdate
-        console.log(day)
-        if (day == 0) day = 'Sunday'
-        else if (day == 1) day = 'Monday'
-        else if (day == 2) day = 'Tuesday'
-        else if (day == 3) day = 'Wednesday'
-        else if (day == 4) day = 'Thursday'
-        else if (day == 5) day = 'Friday'
-        else if (day == 6) day = 'Saturday'
-        console.log(day)
-        let url = DATABASE_URL + '/news/' + day
+        let date_endpoint = (this.state.dtdate.getMonth() + 1) + '-' + this.state.dtdate.getDate() + '-' + this.state.dtdate.getFullYear()         
+        let url = DATABASE_URL + '/news/' + date_endpoint
         let response = await fetch(url)
         let responseJson = await response.json()
         this.setState({
             news_stories: responseJson
         })
-        console.log(this.state)
+        if (responseJson.length == 0) 
+            this.setState({
+            news_stories: null
+        })
+        console.log(responseJson.length)
     }
     goToArticle(item) {
         console.log(item)
         this.props.navigation.navigate('NewsArticle', {item: item})
     }
+    async goToNextDay() {
+        var current = this.state.dtdate
+        var nextDate = current
+        nextDate.setDate(current.getDate() + 1)
+        let date_endpoint = (nextDate.getMonth() + 1) + '-' + nextDate.getDate() + '-' + nextDate.getFullYear()
+        let url = DATABASE_URL + '/news/' + date_endpoint
+        let response = await fetch(url)
+        let responseJson = await response.json()      
+        this.setState({
+            dtdate: nextDate,
+            news_stories: responseJson
+        });
+        if (responseJson.length == 0) 
+            this.setState({
+            news_stories: null
+        })
+        console.log(this.state)
+    }
+    async goToPreviousDay() {
+        var current = this.state.dtdate
+        var prevDate = current
+        prevDate.setDate(current.getDate() - 1)
+        let date_endpoint = (prevDate.getMonth() + 1) + '-' + prevDate.getDate() + '-' + prevDate.getFullYear()
+        let url = DATABASE_URL + '/news/' + date_endpoint
+        let response = await fetch(url)
+        let responseJson = await response.json()      
+        this.setState({
+            dtdate: prevDate,
+            news_stories: responseJson
+        });
+        if (responseJson.length == 0) 
+            this.setState({
+            news_stories: null
+        })
+        console.log(this.state)
+    }
     render() {
+        if (this.state.news_stories == null) {
+            return(
+                <View style={styles.containerError}>
+                <View style = {styles.date}>
+                        <TouchableOpacity
+                        onPress={() => this.goToPreviousDay()}>
+                        <Text> {'<'}</Text>
+                        </TouchableOpacity>
+                        <Text> {this.state.dtdate.toDateString()}</Text>
+                        <TouchableOpacity
+                        onPress={() => this.goToNextDay()}>
+                        <Text> {'>'}</Text>
+                        </TouchableOpacity>
+                    </View>
+                <Text style={{
+                    fontSize: 24, 
+                    marginTop: 50,
+                    fontFamily: "Times New Roman"
+                }}>News Stories</Text>
+
+                <Text>There are no News Stories to Show from Today</Text>
+
+                <TouchableOpacity
+                style={styles.buttonError} 
+                onPress={() => this.props.navigation.navigate('HomeScreen')}>
+                <Text style={styles.buttonText}> Back to Home </Text>
+                </TouchableOpacity>
+                </View>
+            )
+        }
         return (
             <View style={styles.container}>
+            <View style = {styles.date}>
+                        <TouchableOpacity
+                        onPress={() => this.goToPreviousDay()}>
+                        <Text> {'<'}</Text>
+                        </TouchableOpacity>
+                        <Text> {this.state.dtdate.toDateString()}</Text>
+                        <TouchableOpacity
+                        onPress={() => this.goToNextDay()}>
+                        <Text> {'>'}</Text>
+                        </TouchableOpacity>
+            </View>
             <Text style={{
                 fontSize: 24, 
                 marginTop: 50,
@@ -75,6 +147,19 @@ export default class NewsScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    buttonError: {
+        backgroundColor: '#ee6002',
+        borderRadius: 25,
+        width: SCREEN_WIDTH - 100,
+        marginTop: SCREEN_HEIGHT - 380
+    },
+    containerError: {
+        paddingTop: 40,
+        flex: 1, 
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: "white"
+    },
     buttonText: {
         fontSize: 16,
         color: 'white',
@@ -110,5 +195,15 @@ const styles = StyleSheet.create({
     description: {
         fontSize: 16,
         fontFamily: "Times New Roman"
+    },
+    date: {
+        flexDirection: 'row',
+        height: 40,
+        width: SCREEN_WIDTH - 100,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
+        marginTop: 50
     }
 })
